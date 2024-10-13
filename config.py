202b25a2228d7afa1f4f7b3320a5cc6e83c05de1
Ivan Mikhailichenko <159043091+Vanilin1408@users.noreply.github.com>
2024-10-13 19:57:51 +0300

    Small fixes

--- config.py
+++
@@ -1,9 +0,0 @@
-"""
-Файл конфигурации с основными ключами.
-"""
-
-# Главный токен ТГ бота
-BOT_TOKEN: str = "7338043462:AAF0XjFtK1icyi7lZgC5CI5oOqKFyg-1diM"
-
-# Ключ API OpenWeatherMap
-API_KEY: str = "5d3455c18293331c481512a297a57d9e"
